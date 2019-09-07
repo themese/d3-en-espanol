842200a15f415e11f0313a2edf48422aecc35d5f
TheMese <jousema.fernandez@gmail.com>
2019-09-07 12:20:51 +0100

    Installed hookrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import {useRoutes}  from 'hookrouter';
 
 const App: React.FC = () => {
+  const routeResult = useRou
   return (
-    <div>Hello!</div>
+    <VerticalBarChart />
   );
 }
 
