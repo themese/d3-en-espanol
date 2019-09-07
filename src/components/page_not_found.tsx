69a38d62ce94cdbc166e72cd10f0488f835ac3e1
TheMese <jousema.fernandez@gmail.com>
2019-09-07 13:53:34 +0100

    Page not found added

--- src/components/page_not_found.tsx
+++ src/components/page_not_found.tsx
@@ -2,7 +2,17 @@ import React from 'react';
 
 const PageNotFound = () => {
     return (
-        <div> Page not found!</div>
+        <div className='page-not-found'>
+            <div id='title'>Sorry, couldn't find what you are looking for!</div>
+            <div className='circles'>
+                <p>404<br />
+                    <small>PAGE NOT FOUND</small>
+                </p>
+                <span className='circle big'></span>
+                <span className='circle med'></span>
+                <span className='circle small'></span>
+            </div>
+        </div>
     );
 };
 
